f286fa8b03a1315c7ad249b6bd6ee9c4989df96a
vkuznetsoff <ss_salvich@mail.ru>
2024-12-17 11:16:39 +0300

    refactor: delete old files, restart storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { icons } from '@/components/Sidebar/data'
+import { Sidebar } from '@/components/Sidebar/sidebar'
+
 export default function Home() {
-  return <div>main page</div>
+  return (
+    <div>
+      main page
+      <Sidebar icons={icons} />
+    </div>
+  )
 }
