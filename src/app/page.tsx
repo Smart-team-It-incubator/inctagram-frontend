fa73153048dfb53ff9a85e6258cb6de986f20bb6
vkuznetsoff <ss_slavich@mail.ru>
2024-12-18 19:33:07 +0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Menu } from "@/components/Menu/Menu";
-import { icons } from "@/components/Sidebar/data";
-import {  Sidebar } from "@/components/Sidebar/sidebar";
+import { Menu } from '@/components/Menu/Menu'
+import { icons } from '@/components/Sidebar/data'
+import { Sidebar } from '@/components/Sidebar/sidebar'
 
 export default function Home() {
-  return <div>main page 
-    <Menu/> 
-    <Sidebar icons={icons} />
-  </div>
+  return (
+    <div>
+      main page
+      <Menu />
+      <Sidebar icons={icons} />
+    </div>
+  )
 }
