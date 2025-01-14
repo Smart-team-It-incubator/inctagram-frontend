c81dc14398c581098cc959acb2e0fd99aa5ae165
«Elena <LenaRybinskova@gmail.com>
2025-01-14 10:53:31 +0300

    SCRUM-22 init: start validation

--- src/features/SignUp/SignUp.tsx
+++ src/features/SignUp/SignUp.tsx
@@ -11,6 +11,7 @@ import styles from './signUp.module.scss'
 
 import { GithubIcon } from '../../../public/icons/GithubIcon'
 import { GoogleIcon } from '../../../public/icons/GoogleIcon'
+import { useController } from 'react-hook-form'
 
 type FormValue = {
   email: string
@@ -20,9 +21,22 @@ type FormValue = {
 }
 
 export const SignUp = () => {
-  const { handleSubmit, register } = useForm<FormValue>()
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    trigger,
+    control,
+  } = useForm<FormValue>()
+  console.log('errors:', errors)
   const onSubmit: SubmitHandler<FormValue> = data => console.log(data)
   const router = useRouter()
+
+  const { field, fieldState } = useController({
+    name,
+    control,
+    rules: options,
+  })
 
   return (
     <div className={styles.container}>
@@ -39,29 +53,77 @@ export const SignUp = () => {
           textPlaceholder={'Epam11'}
           title={'Username'}
           type={'text'}
-          {...register('username', {required: true })}
+          onBlur={() => trigger('username')}
+          errorMessage={errors?.username?.message}
+          {...register('username', {
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+            maxLength: {
+              value: 30,
+              message: 'Maximum number of characters 30',
+            },
+            minLength: {
+              value: 6,
+              message: 'Minimum number of characters 6',
+            },
+          })}
         />
         <CustomInput
           autoComplete={'new-password'}
           textPlaceholder={'Epam@epam.com'}
           title={'Email'}
           type={'text'}
-          {...register('email', { required: true })}
+          errorMessage={errors?.email?.message}
+          {...register('email', {
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+            pattern: { value: /@/, message: 'The email must match the format example@example.com' },
+          })}
         />
         <CustomInput
           autoComplete={'new-password'}
           textPlaceholder={'******************'}
           title={'Password'}
           type={'password'}
-          {...register('password', {  required: true })}
+          errorMessage={errors?.password?.message}
+          {...register('password', {
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+            pattern: {
+              value: /^[0-9A-Za-z!"#$%&'()*+,\-./:;<=>?@[\]^_`{|}~]+$/,
+              message: 'error',
+            },
+            minLength: {
+              value: 6,
+              message: 'Minimum number of characters 6',
+            },
+          })}
         />
         <CustomInput
           autoComplete={'new-password'}
           textPlaceholder={'******************'}
           title={'Password confirmation'}
           type={'password'}
+          errorMessage={errors?.passwordConfirmation?.message}
           {...register('passwordConfirmation', {
-            required: true,
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+            pattern: {
+              value: /^[0-9A-Za-z!"#$%&'()*+,\-./:;<=>?@[\]^_`{|}~]+$/,
+              message: 'error',
+            },
+            minLength: {
+              value: 6,
+              message: 'Minimum number of characters 6',
+            },
           })}
         />
         {/*Checkbox*/}
