e633e3470c7255605767c39af80c0cb174ed77e5
vkuznetsoff <ss_slavich@mail.ru>
2025-01-22 14:10:06 +0300

    fix: remove comments and unwork code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
 'use client'
 export default function Home() {
-  // const queryString = 'https://auth.smart-reg.org.ru/api/v1/auth/github/callback'
-  // const urlParams = new URLSearchParams(queryString)
-  // const accessToken = urlParams.get('access-token')
-  // console.log('urlParams ', urlParams)
-  const s = window.location.search
-
-  const urlParams = new URLSearchParams(s);
-const code = urlParams.get('code');
-console.log('Authorization code:', s);
-
-
   return <div>Home</div>
 }
