69fc410e2b3e2fac9482d913e3f6175448868958
dreammc98 <dreammc98@gmail.com>
2025-01-23 21:35:36 +0700

    fix auth/newPassword

--- src/app/auth/newPassword/page.tsx
+++ src/app/auth/newPassword/page.tsx
@@ -1,9 +1,23 @@
-import { NewPassword } from '@/features/NewPassword/NewPassword'
+'use client'
+// import { NewPassword } from '@/features/NewPassword/NewPassword'
+import dynamic from 'next/dynamic'
+ 
+const DynamicComponentWithNoSSR = dynamic(
+  async () => {
+    const module = await import('../../../features/NewPassword/NewPassword');
+    return module.NewPassword;
+  },
+
+  { ssr: false }
+)
+
+
+
 
 export default function forgotPassword() {
   return (
     <>
-      <NewPassword />
+      <DynamicComponentWithNoSSR/>
     </>
   )
 }
