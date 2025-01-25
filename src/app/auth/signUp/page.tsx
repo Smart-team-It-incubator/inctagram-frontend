1674add57d9815df0ed65480f473b69c854e5987
dreammc98 <dreammc98@gmail.com>
2025-01-25 11:09:00 +0700

    fix signUp/page.tsx

--- src/app/auth/signUp/page.tsx
+++ src/app/auth/signUp/page.tsx
@@ -1,10 +1,22 @@
+'use client'
+
 import { Container } from '@/components/shared/Container'
-import { SignUp } from '@/features/SignUp'
+// import { SignUp } from '@/features/SignUp'
+import dynamic from 'next/dynamic'
 
 export default function Auth() {
+  const DynamicComponentWithNoSSR = dynamic(
+    async () => {
+      const module = await import('../../../features/SignUp/SignUp')
+      return module.SignUp
+    },
+
+    { ssr: false }
+  )
+
   return (
     <Container margin={'24px auto'} maxWidth={'378px'}>
-      <SignUp />
+      <DynamicComponentWithNoSSR />
     </Container>
   )
 }
