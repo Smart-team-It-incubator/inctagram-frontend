66923fc090211f5d61189e19efb894f391c86331
vkuznetsoff <ss_slavich@mail.ru>
2025-01-27 00:36:36 +0300

    feat: add cropImage function and new props, need to fix. add cursor to ImgBlock in tooltip, fix select image and update Crop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client'
-import { Maximize } from '@/components/icons'
 import { AddPhotoModal } from '@/features/AddPost/AddPhoto/AddPhotoModal'
-import { Crop } from '@/features/AddPost/CropPhoto/Crop'
-import { PhotoEditor } from '@/features/AddPost/CropPhoto/PhotoEditor'
+
 
 import { ModalWindow } from '@/features/AddPost/ModalWindow'
 
--- src/features/AddPost/CropPhoto/Crop.tsx
+++ src/features/AddPost/CropPhoto/Crop.tsx
@@ -8,6 +8,7 @@ import { CloseOutline, Expand, Image, Maximize, PlusCircleOutline } from '@/comp
 
 import { IconButton, Slider } from '@radix-ui/themes'
 import { CustomSlider } from '../Slider/Slider'
+import getCroppedImg from './cropImage'
 
 const URL =
   'https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000'
@@ -20,38 +21,55 @@ type CropProps = {
 type Tools = 'zoom' | 'aspect' | 'image'
 
 type ToolsVisibility = {
-  'zoom': boolean 
-  'aspect': boolean
-  'image': boolean
+  zoom: boolean
+  aspect: boolean
+  image: boolean
 }
 
 const initToolsVisibility = {
-  'zoom': false, 
-  'aspect': false,
-  'image': false,
+  zoom: false,
+  aspect: false,
+  image: false,
 }
 
 export const Crop = ({ images, uploadPhoto }: CropProps) => {
   const [crop, setCrop] = useState<Point>({ x: 0, y: 0 })
   const [zoom, setZoom] = useState<number>(1)
   const [selectedImage, setSelectedImage] = useState<number>(0)
-  
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(null)
+  const [croppedImage, setCroppedImage] = useState<string | null>(null)
 
   const [showTooltip, setShowTooltip] = useState<ToolsVisibility>(initToolsVisibility)
-  
+
   const cropChange = (crop: Point) => {
     setCrop(crop)
   }
 
+  const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
+    setCroppedAreaPixels(croppedAreaPixels)
+  }
+
+  const cropImage = async (image: string, croppedAreaPixels: Area) => {
+    console.log('crop')
+    try {
+      const croppedImage = await getCroppedImg(image, croppedAreaPixels)
+
+      console.log('donee', { croppedImage })
+
+      setCroppedImage(croppedImage)
+    } catch (e) {
+      console.error(e)
+    }
+  }
   const openImageList = () => {
     setSelectedImage(0)
   }
 
   const showTooltipHandle = (sourse: Tools): void => {
-    console.log('sourse: ', sourse)
-    console.log('showTooltip: ', showTooltip)
-    console.log('!showTooltip[sourse]}: ', !showTooltip[sourse])
-    setShowTooltip({...showTooltip, [sourse]: !showTooltip[sourse]})
+    // console.log('sourse: ', sourse)
+    // console.log('showTooltip: ', showTooltip)
+    // console.log('!showTooltip[sourse]}: ', !showTooltip[sourse])
+    setShowTooltip({ ...showTooltip, [sourse]: !showTooltip[sourse] })
   }
 
   return (
@@ -66,6 +84,7 @@ export const Crop = ({ images, uploadPhoto }: CropProps) => {
           crop={crop}
           aspect={4 / 3}
           onCropChange={cropChange}
+          onCropComplete={onCropComplete}
           onZoomChange={setZoom}
         />
 
@@ -73,26 +92,39 @@ export const Crop = ({ images, uploadPhoto }: CropProps) => {
           <div className={styles.buttons}>
             <div className={styles.buttonsLeft}>
               <div className={styles.iconContainer}>
-                <Expand width={24} height={24} className={styles.icon} onClick={() => showTooltipHandle('aspect')}/>
+                <Expand
+                  width={24}
+                  height={24}
+                  className={styles.icon}
+                  onClick={() => showTooltipHandle('aspect')}
+                />
               </div>
 
-              {showTooltip['aspect'] && <div className="aspect">Aspect</div> }
+              {showTooltip['aspect'] && <div className="aspect">Aspect</div>}
 
               <div className={styles.iconContainer}>
-                <Maximize width={24} height={24} className={styles.icon} onClick={() => showTooltipHandle('zoom')}/>
+                <Maximize
+                  width={24}
+                  height={24}
+                  className={styles.icon}
+                  onClick={() => showTooltipHandle('zoom')}
+                />
               </div>
 
               {showTooltip['zoom'] && <CustomSlider setValue={setZoom} />}
-
             </div>
 
-            {showTooltip['image'] && <ImageListTooltip
-              images={images}
-              uploadPhoto={uploadPhoto}
-              setSelectedImage={setSelectedImage}
-              selectedImage={selectedImage}
-              setZoom={setZoom}
-            />}
+            {showTooltip['image'] && (
+              <ImageListTooltip
+                images={images}
+                uploadPhoto={uploadPhoto}
+                setSelectedImage={setSelectedImage}
+                selectedImage={selectedImage}
+                setZoom={setZoom}
+                cropImage={cropImage}
+                croppedAreaPixels={croppedAreaPixels}
+              />
+            )}
 
             <div className={styles.iconContainer} onClick={() => showTooltipHandle('image')}>
               <Image width={24} height={24} className={styles.icon} onClick={openImageList} />
@@ -108,8 +140,10 @@ type TooltipProps = {
   images: ImageType[]
   uploadPhoto: Function
   setSelectedImage: Function
-  selectedImage: number,
+  selectedImage: number
   setZoom?: Function
+  cropImage: Function
+  croppedAreaPixels: Area | null
 }
 
 const ImageListTooltip = ({
@@ -117,8 +151,12 @@ const ImageListTooltip = ({
   uploadPhoto,
   setSelectedImage,
   selectedImage,
-  setZoom
+  setZoom,
+  cropImage,
+  croppedAreaPixels,
 }: TooltipProps) => {
+  const [imageIndex, setImageIndex] = useState<number>(0)
+
   const addImage = async () => {
     await uploadPhoto()
     setSelectedImage((prev: number) => prev + 1)
@@ -126,19 +164,21 @@ const ImageListTooltip = ({
     setZoom && setZoom(1)
   }
 
-  const selectImageFromTooltip = (id: string) => {}
+  const selectImageFromTooltip = (index: number) => {
+    setSelectedImage(index)
+  }
 
   return (
     <div className={styles.imagesTooltip}>
       <Slider defaultValue={[50]} />
       <div className={styles.imageList}>
-        {images?.map(image => {
+        {images?.map((image, index) => {
           return (
             <div className={styles.imageBlock} key={image.id}>
               <img
                 src={image.croppedImageUrl ? image.croppedImageUrl : image.imageUrl}
                 alt="image"
-                onClick={() => selectImageFromTooltip(image.id)}
+                onClick={() => selectImageFromTooltip(index)}
               />
               <div className={styles.closeBtn}>
                 <div className={`${styles.iconContainer}`}>
@@ -161,6 +201,12 @@ const ImageListTooltip = ({
       <div className={styles.tooltipControls}>
         <div className={styles.iconContainer}>
           <PlusCircleOutline className={styles.icon} onClick={addImage} />
+          <div
+            onClick={() => cropImage(images[imageIndex].imageUrl, croppedAreaPixels)}
+            style={{ cursor: 'pointer' }}
+          >
+            V
+          </div>
         </div>
       </div>
     </div>
