5186bdad0a95953b79f67c70d0bcd90089ed2b60
«Elena <LenaRybinskova@gmail.com>
2025-02-04 17:16:41 +0300

    SCRUM-38: add CardsList component

--- src/common/api/users/usersApi.ts
+++ src/common/api/users/usersApi.ts
@@ -2,14 +2,5 @@ import {baseApi} from '@/common/api/baseApi';
 
 
 export const userApi = baseApi.injectEndpoints({
-    endpoints: build => ({
-/*        registration: build.query<User, void>({
-            query: () => {
-                return {
-                    method: 'GET',
-                    url:API_URLS.USER.GET_ALL_USERS,
-                }
-            },
-        }),*/
-    }),
+    endpoints: () => ({}),
 })
