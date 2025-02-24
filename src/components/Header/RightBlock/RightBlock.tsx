ceaf26f8864a2bf11e9280e422f93f7731ae3d89
dreammc98 <dreammc98@gmail.com>
2025-02-24 22:03:59 +0700

    fix: change header

--- src/components/Header/RightBlock/RightBlock.tsx
+++ src/components/Header/RightBlock/RightBlock.tsx
@@ -1,31 +1,38 @@
-import Image from 'next/image'
-import { Button } from '@/components/Button'
-import { CustomAccordion } from './CustomAccordion'
+'use client'
 
-import styles from './RightBlock.module.scss'
+import { Button } from '@/components/Button'
+import s from './RightBlock.module.scss'
+import { usePathname } from 'next/navigation'
+import { AUTH } from '@/common/routes/routes'
+import { Notice } from './Notice/Notice'
+import { Dropdown } from './Dropdown/Dropdown'
 
 type Props = {
   isAuth?: boolean
 }
 
-export const RightBlock = ({ isAuth }: Props) => {
+export const RightBlock = ({ isAuth = false }: Props) => {
+  const validateButtons = isAuthPage() || isAuth
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.wrapper}>
-        {isAuth && (
-          <Image src="/Mask.svg" alt="Bell" width={24} height={24} className={styles.bell} />
-        )}
-        <CustomAccordion />
-        {!isAuth && (
-          <>
-            <div className={styles.hideOnMobile}>
-              <Button variant="link">Log in</Button>
-              <Button>Sign up</Button>
-            </div>
-            <span className={styles.ellipsis}>...</span>
-          </>
-        )}
-      </div>
+    <div className={s.wrapper}>
+      {!validateButtons && (
+        <>
+          {isAuth && <Notice />}
+
+          <div className={s.hideOnMobile}>
+            <Button variant="link">Log in</Button>
+            <Button>Sign up</Button>
+          </div>
+        </>
+      )}
+      {!isAuthPage() && <Dropdown />}
     </div>
   )
+}
+
+export const isAuthPage = () => {
+  const pathname = usePathname()
+
+  return Object.values(AUTH).some(path => pathname.startsWith(path))
 }
